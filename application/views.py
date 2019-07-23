4eec0f65dd68b559d607b14fc891c74a0b3c77c6
Allen <shaoaallen@163.com>
2019-07-23 20:58:13 +0800

    半成品：删除，新增已完成（明天美化），编辑写了一半

--- application/views.py
+++ application/views.py
@@ -1,5 +1,7 @@
 from application import app, db
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from pymongo import MongoClient
+
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +20,45 @@ def index():
         return render_template('error.html', message=err_msg)
 
     return render_template('main.html', docs=docs)
+
+
+# @app.route('/insert', methods=['POST', 'GET'])
+# def insert():
+#     db.data.insert()
+
+
+@app.route('/delete/<string:doc_id>', methods=['POST', 'GET'])
+def delete(doc_id):
+    db.data.remove({'病历号': doc_id})
+    return redirect(url_for('index'))
+
+
+@app.route('/update/<string:doc_id>', methods=['POST', 'GET'])
+def update(doc_id):
+    db.data.update({'病历号': doc_id}, {'医院': '省人民医院'})
+    return redirect(url_for('index'))
+
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
+
+
+@app.route('/to_insert')
+def to_insert():
+    return render_template('insert.html')
+
+
+@app.route('/insert', methods=['POST', 'GET'])
+def insert():
+    dict = {'医院': request.form['hospital'],
+            '姓名': request.form['name'],
+            '性别 ': request.form['gender'],
+            '出生日期 ': request.form['bir'],
+            '入院时间 ': request.form['in_time'],
+            '病历号': request.form['record_id']
+            }
+    print(dict)
+    db.data.insert(dict)
+    return redirect(url_for('index'))
+
